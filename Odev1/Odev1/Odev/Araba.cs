08c6fb7a2d7c84cfdf7485db475d3caee90cd5f4
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-02-23 14:33:31 +0300

    Ödev1

---
+++ Odev1/Odev1/Odev/Araba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1.Odev
+{
+    public class Araba
+    {
+        public void vites_yazdir(string marka, string vites)
+        {
+            Console.WriteLine(marka + vites + " viteslidir.");
+        }
+    }
+}
---
+++ Odev1/Odev1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Odev1.Odev;
+
+Audi audi = new Audi();
+audi.vites_yazdir("Audi", " otomatik");
+
+Bmw bmw = new Bmw();
+bmw.vites_yazdir("Bmw", " duz");
+
+Mercedes mercedes = new Mercedes();
+mercedes.vites_yazdir("Mercedes", " otomatik");
+
+Porsche porsche = new Porsche();
+porsche.vites_yazdir("Porsche", " otomatik");
+
+Togg togg = new Togg();
+togg.vites_yazdir("Togg", " otomatik");
+
+Toyota toyota = new Toyota();
+toyota.vites_yazdir("Toyota", " duz");
