b878f3fb9a6e39fb370f63a0d011b24eec3a9476
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-03-15 13:42:26 +0300

    Add files via upload

---
+++ Odev(18.03.2025)/Odev1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Odev1;
+
+Marka marka = new Marka("BMW");
+Model model = new Model("X5");
+Kapi kapi = new Kapi(4);
+Pencere pencere = new Pencere(4);
+Kasa kasa = new Kasa("Suv");
+int fiyat = 2000000;
+
+Araba araba = new Araba(marka, model, kapi, pencere, kasa, fiyat);
+araba.bilgileriGoster();
---
+++ Odev(18.03.2025)/Odev2/Program.cs
@@ -0,0 +1,8 @@
+﻿using Odev2;
+
+Matematik matematik = new Matematik();
+
+matematik.topla(2.5, 3.5);
+matematik.topla(3, 4, 5);
+matematik.carp(3, 2);
+matematik.carp(1, 2, 4);
