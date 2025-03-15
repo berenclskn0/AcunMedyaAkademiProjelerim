a3a40e62481d52262409e6abe881ea7a1a8c004b
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-03-15 13:31:52 +0300

    Odev

---
+++ Odevler 2/Odev1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Odev1;
+
+Insan insan = new Insan();
+int yas = insan.yasHesapla(2003);
+string sonuc = insan.yasBelirleme(yas);
+Console.WriteLine($"Yaşınız: {yas} --> {sonuc}");
---
+++ Odevler 2/Odev5/Program.cs
@@ -0,0 +1,33 @@
+﻿using Odev5;
+
+Araba[] arabalar = new Araba[2];
+
+arabalar[0] = new Araba
+{
+    marka = "Toyota",
+    model = "Chr",
+    depoKapasitesi = 40,
+    gidilenYol = 600
+};
+
+arabalar[0].benzinTuketimi = arabalar[0].benzinHesapla();
+
+arabalar[1] = new Araba
+{
+    marka = "Honda",
+    model = "Civic",
+    depoKapasitesi = 50,
+    gidilenYol = 700
+};
+
+arabalar[1].benzinTuketimi = arabalar[1].benzinHesapla();
+
+double toplamBenzin = 0;
+
+for (int i = 0; i < arabalar.Length; i++)
+{
+    arabalar[i].bilgileriGoster();
+    toplamBenzin += arabalar[i].benzinTuketimi;
+}
+
+Console.WriteLine($"Toplam Benzin Tuketimi: {toplamBenzin}L/Km");
