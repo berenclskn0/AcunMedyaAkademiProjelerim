cc6f0a128b2a11947ba83251c2a43acb19a5af48
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-03-21 20:29:25 +0300

    Add files via upload

---
+++ Odev_NamespaceVeGenisletilebilirlik/Odev_NamespaceVeGenisletilebilirlik/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatematikIslemleri;
+
+namespace MatematikIslemleri
+{
+    public class Hesaplama
+    {
+        public int toplama(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int carpma(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
+
+namespace Odev_NamespaceVeGenisletilebilirlik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesaplama hesaplama = new Hesaplama();
+
+            int toplamSonucu = hesaplama.toplama(2, 3);
+            int carpimSonucu = hesaplama.carpma(2, 3);
+
+            Console.WriteLine($"Toplam: {toplamSonucu}");
+            Console.WriteLine($"Çarpım: {carpimSonucu}");
+        }
+    }
+}
