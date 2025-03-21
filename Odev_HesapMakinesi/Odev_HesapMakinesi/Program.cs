16fd90e4ba710db1393fdbb38f3db43c88852ad6
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-03-21 19:56:46 +0300

    Add files via upload
     Kullanıcıdan iki sayı alıp, toplama, çıkarma, çarpma ve bölme işlemlerini yapan bir program.

---
+++ Odev_HesapMakinesi/Odev_HesapMakinesi/Program.cs
@@ -0,0 +1,51 @@
+﻿while (true)
+{
+    Console.Write("Birinci Sayiyi Giriniz: ");
+    int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ikinci Sayiyi Giriniz: ");
+    int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Yapmak istediginiz islemi seciniz:\n" +
+        "'+' : Toplama\n" +
+        "'-' : Cikarma\n" +
+        "'*' : Carpma\n" +
+        "'/' : Bolme\n" +
+        "'q' : Cikis");
+    Console.Write("Seciminiz:");
+    char islem = Convert.ToChar(Console.ReadLine());
+
+    if (islem == '+')
+    {
+        Console.WriteLine($"Sonuc: {sayi1 + sayi2}");
+    }
+    else if (islem == '-')
+    {
+        Console.WriteLine($"Sonuc: {sayi1 - sayi2}");
+    }
+    else if (islem == '*')
+    {
+        Console.WriteLine($"Sonuc: {sayi1 * sayi2}");
+    }
+    else if (islem == '/')
+    {
+        if (sayi2 != 0)
+        {
+            Console.WriteLine($"Sonuc: {sayi1 / sayi2}");
+        }
+        else
+        {
+            Console.WriteLine("Sifira bolme islemi yapilamaz.");
+        }
+    }
+    else if (islem == 'q' || islem == 'Q')
+    {
+        Console.WriteLine("Programdan cikiliyor.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Gecersiz islem!!");
+    }
+    Console.WriteLine();
+}
