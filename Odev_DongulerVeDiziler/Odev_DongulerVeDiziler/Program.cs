b3e3b20706ecbdf501b010cbdce61667a99e03c7
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-03-21 18:13:56 +0300

    Add files via upload
    Kullanıcıdan 5 adet sayı alarak bu sayıların ortalamasını hesaplayan bir program.

---
+++ Odev_DongulerVeDiziler/Odev_DongulerVeDiziler/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] sayilar = new double[5];
+        
+double toplam = 0;
+
+Console.WriteLine("Lütfen 5 tane sayı giriniz");
+
+for (int i = 0; i < sayilar.Length; i++)
+{
+    Console.Write($"Sayı {i + 1}: ");
+            
+    sayilar[i] = Convert.ToDouble(Console.ReadLine());
+            
+    toplam += sayilar[i];
+}
+
+double ortalama = toplam / sayilar.Length;
+
+Console.WriteLine($"Girilen sayıların ortalaması: {ortalama}");
