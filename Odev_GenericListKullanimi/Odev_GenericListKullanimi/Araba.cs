666f0d19adb2ebbcf9247f1c0e0a4ee452d4a591
Yasemin Beren Çalışkan <153419983+berenclskn0@users.noreply.github.com>
2025-03-24 18:17:05 +0300

    Add files via upload

---
+++ Odev_GenericListKullanimi/Odev_GenericListKullanimi/Araba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_GenericListKullanimi
+{
+    public class Araba
+    {
+        public string marka;
+        public string model;
+        public double benzinHarcamasi;
+        public double toplamMesafe;
+
+        public double ToplamYakitTuketimi()
+        {
+            return (toplamMesafe / 100) * benzinHarcamasi;
+        }
+    }
+}
---
+++ Odev_GenericListKullanimi/Odev_GenericListKullanimi/Program.cs
@@ -0,0 +1,32 @@
+﻿using Odev_GenericListKullanimi;
+
+List<Araba> arabalar = new List<Araba>();
+
+arabalar.Add(new Araba
+{
+    marka = "Toyota",
+    model = "Corolla",
+    benzinHarcamasi = 6.5,
+    toplamMesafe = 5000
+});
+
+arabalar.Add(new Araba
+{
+    marka = "BMW",
+    model = "X5",
+    benzinHarcamasi = 9.0,
+    toplamMesafe = 5000
+});
+
+arabalar.Add(new Araba
+{
+    marka = "Mercedes",
+    model = "C200",
+    benzinHarcamasi = 8.2,
+    toplamMesafe = 5000
+});
+
+foreach(Araba araba in arabalar)
+{
+    Console.WriteLine($"Marka: {araba.marka}, Model: {araba.model}, 100 km'de Yaktığı Yakıt: {araba.benzinHarcamasi} litre, Toplam Yakıt Tüketimi: {araba.ToplamYakitTuketimi()} litre");
+}
